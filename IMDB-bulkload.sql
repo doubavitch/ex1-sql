7d818a9f40bbccc6d386eb138009b482c0837c88
Yanis Bosch <yanis.bosch@gmail.com>
2023-10-20 14:51:02 +0200

    Exercise 3 a and b solved by Clara

--- IMDB-bulkload.sql
+++ IMDB-bulkload.sql
@@ -3,122 +3,122 @@
 -- 2) the first header line (with the column names) needs to be removed for the COPY commands to work properly
 
 -- NOTE: The code to import the data into Persons, Titles,... is commented to avoid having to run
--- intensive code multiple times, DONT FORGET TO UNCOMMENT FOR SUBMISSION
+--			intensive code multiple times, DONT FORGET TO UNCOMMENT FOR SUBMISSION
 
 --Exercise 1 a) ----------------------------------------------------------------------
 
 DROP TABLE IF EXISTS Persons;
 CREATE TABLE Persons (
- nid char(10),
- primaryName varchar(128),
- birthYear int,
- deathYear int,
- primaryProfession varchar(128),
- knownForTitles varchar(128));
-COPY Persons FROM '/tmp/name.basics.tsv' NULL '\N' ENCODING 'UTF8' DELIMITER E'\t' QUOTE E'\b' CSV HEADER;
+	nid char(10),
+	primaryName varchar(128),
+	birthYear int,
+	deathYear int,
+	primaryProfession varchar(128),
+	knownForTitles varchar(128));
+COPY Persons FROM '/tmp/name.basics.tsv' NULL '\N' ENCODING 'UTF8';
 SELECT * FROM Persons LIMIT 100;
 
 DROP TABLE IF EXISTS Titles;
 CREATE TABLE Titles (
- tid char(10),
- ttype varchar(12),
- primaryTitle varchar(1024),
- originalTitle varchar(1024),
- isAdult int,
- startYear int,
- endYear int,
- runtimeMinutes int,
- genres varchar(256));
-COPY Titles FROM '/tmp/title.basics.tsv' NULL '\N' ENCODING 'UTF8' DELIMITER E'\t' QUOTE E'\b' CSV HEADER;
+	tid char(10),
+	ttype varchar(12),
+	primaryTitle varchar(1024),
+	originalTitle varchar(1024),
+	isAdult int,
+	startYear int,
+	endYear int,
+	runtimeMinutes int,
+	genres varchar(256));
+COPY Titles FROM '/tmp/title.basics.tsv' NULL '\N' ENCODING 'UTF8';
 SELECT * FROM Titles LIMIT 100;
-     
+			  
 DROP TABLE IF EXISTS Principals;
 CREATE TABLE Principals (
- tid char(10),
- ordering int,
- nid char(10),
- category varchar(32),
- job varchar(512),
- characters varchar(2048));
-COPY Principals FROM '/tmp/title.principals.tsv' NULL '\N' ENCODING 'UTF8' DELIMITER E'\t' QUOTE E'\b' CSV HEADER;
+	tid char(10),
+	ordering int,
+	nid char(10),
+	category varchar(32),
+	job varchar(512),
+	characters varchar(2048));
+COPY Principals FROM '/tmp/title.principals.tsv' NULL '\N' ENCODING 'UTF8';
 SELECT * FROM Principals LIMIT 100;
-    
+		  
 DROP TABLE IF EXISTS Ratings;
 CREATE TABLE Ratings (
         tid char(10),
         avg_rating numeric,
         num_votes numeric);
-COPY Ratings FROM '/tmp/title.ratings.tsv' NULL '\N' ENCODING 'UTF8' DELIMITER E'\t' QUOTE E'\b' CSV HEADER;
-SELECT * FROM Ratings LIMIT 100; 
+COPY Ratings FROM '/tmp/title.ratings.tsv' NULL '\N' ENCODING 'UTF8';
+SELECT * FROM Ratings LIMIT 100;
 
 -- Exercise 1 b) ----------------------------------------------------------------------
 
 DROP Table IF EXISTS Directs;
 CREATE TABLE Directs(
- nid char(10),
- tid char(10)
+	nid char(10),
+	tid char(10)
 );
 
 DROP Table IF EXISTS Starsin;
 CREATE TABLE Starsin(
- nid char(10),
- tid char(10)
+	nid char(10),
+	tid char(10)
 );
 
 DROP Table IF EXISTS Movie;
 CREATE TABLE Movie(
- tid char(10),
- title varchar(1024),
- year int,
- length int,
- rating numeric
+	tid 	char(10),
+	title 	varchar(1024),
+	year	int,
+	length	int,
+	rating	numeric
 );
 
 DROP Table IF EXISTS Director;
 CREATE TABLE Director(
- nid char(10),
- name varchar(128),
- birthYear int,
- deathYear int
+	nid 		char(10),
+	name		varchar(128),
+	birthYear	int,
+	deathYear	int
 );
 
 DROP Table IF EXISTS Actor;
 CREATE TABLE Actor(
- nid char(10),
- name varchar(128),
- birthYear int,
- deathYear int
+	nid			char(10),
+	name		varchar(128),
+	birthYear	int,
+	deathYear	int
 );
 
 -- Exercise 1 c) ----------------------------------------------------------------------
 
 DELETE FROM Movie WHERE TRUE;
 INSERT INTO Movie (SELECT DISTINCT Titles.tid AS tid, Titles.originaltitle AS Title,Titles.runtimeMinutes AS length, Titles.startYear AS year, Ratings.avg_rating AS rating
-      FROM Titles,Ratings
-      WHERE Titles.tid = Ratings.tid AND Titles.ttype='movie' AND Ratings.num_votes >= 10000
-      ORDER BY Ratings.avg_rating DESC
-      LIMIT 5000);
+				  FROM Titles,Ratings
+				  WHERE Titles.tid = Ratings.tid AND Titles.ttype='movie' AND Ratings.num_votes >= 10000
+				  ORDER BY Ratings.avg_rating DESC
+				  LIMIT 5000);
 
 DELETE FROM Directs WHERE TRUE;
 INSERT INTO Directs(SELECT DISTINCT Principals.nid as nid, Principals.tid as tid
-       FROM Principals,Movie
-       WHERE Principals.category = 'director' AND Movie.tid = Principals.tid);
-       
+				   FROM Principals,Movie
+				   WHERE Principals.category = 'director' AND Movie.tid = Principals.tid);
+				   
 DELETE FROM Starsin WHERE TRUE;
 INSERT INTO Starsin(SELECT DISTINCT Principals.nid as nid, Principals.tid as tid 
-       FROM Principals,Movie
-       WHERE Movie.tid = Principals.tid AND (Principals.category = 'actor' OR Principals.category = 'actress'));
+				   FROM Principals,Movie
+				   WHERE Movie.tid = Principals.tid AND (Principals.category = 'actor' OR Principals.category = 'actress'));
 
 DELETE FROM Actor WHERE TRUE;
 INSERT INTO Actor (SELECT DISTINCT Persons.nid AS nid, primaryName AS name, Persons.birthYear AS birthYear, Persons.deathYear AS deathYear 
-       FROM Persons,Starsin
-       WHERE Persons.nid = Starsin.nid);
-       
+				   FROM Persons,Starsin
+				   WHERE Persons.nid = Starsin.nid);
+				   
 DELETE FROM Director WHERE TRUE;
 INSERT INTO Director (SELECT DISTINCT Persons.nid AS nid, primaryName AS name, birthYear AS birthYear, deathYear AS deathYear 
-       FROM Persons,Directs
-       WHERE Directs.nid = Persons.nid);
-       
+				   FROM Persons,Directs
+				   WHERE Directs.nid = Persons.nid);
+				   
 -- EXERCISE 1 d) ----------------------------------------------------------------------
 /*NOTE: We suppose that no two movies have the same name in the same year
 We could assume that no two persons in the persons table have the same name 
@@ -157,7 +157,6 @@ ADD CONSTRAINT PK_Director PRIMARY KEY (nid);
 ALTER TABLE Actor
 ADD CONSTRAINT PK_Actor PRIMARY KEY (nid);
 
-
 -- Define the FOREIGN KEY constraints with CASCADE options
 
 ALTER TABLE Directs
@@ -187,7 +186,7 @@ ON UPDATE CASCADE;
 
 -- EXERCISE 2 b) ----------------------------------------------------------------------
 --Have a look at nid of Steven Spielberg before the operation
-SELECT * FROM Director WHERE name = 'Steven Spielberg';
+--SELECT * FROM Director WHERE name = 'Steven Spielberg';
 --the nid is "nm0000229 "
 
 -- To update the nid of Steven Spielberg
@@ -196,10 +195,8 @@ SET nid = '123456789'
 WHERE name = 'Steven Spielberg';
 
 --Have a look at nid of Steven Spielberg after the operation
-SELECT * FROM Director WHERE name = 'Steven Spielberg';
+--SELECT * FROM Director WHERE name = 'Steven Spielberg';
 --the nid is "123456789"
-
-
 
 -- EXERCISE 2 c) ----------------------------------------------------------------------
 -- To delete the actor and director Robert De Niro
@@ -208,3 +205,61 @@ WHERE name = 'Robert De Niro';
 
 DELETE FROM Director
 WHERE name = 'Robert De Niro';
+
+-- Exercise 3 a) ----------------------------------------------------------------------
+SELECT Director.name, COUNT(*) AS Directed_Movies
+FROM Director
+LEFT JOIN Directs ON Director.nid = Directs.nid
+LEFT JOIN Actor ON Director.nid = Actor.nid
+WHERE Actor.nid IS NULL
+GROUP BY Director.name
+ORDER BY Directed_Movies DESC
+LIMIT 25;
+
+-- Exercise 3 b) ----------------------------------------------------------------------
+SELECT Actor1.name AS Actor1, Actor2.name AS Actor2, COUNT(*) AS Co_occurrences
+FROM Starsin AS S1
+JOIN Starsin AS S2 ON S1.tid = S2.tid AND S1.nid < S2.nid
+JOIN Actor AS Actor1 ON S1.nid = Actor1.nid
+JOIN Actor AS Actor2 ON S2.nid = Actor2.nid
+GROUP BY Actor1.name, Actor2.name
+ORDER BY Co_occurrences DESC
+LIMIT 25;
+
+-- Exercise 3 c) ----------------------------------------------------------------------
+
+DROP Table IF EXISTS UnionActorDirector;
+CREATE TABLE UnionActorDirector AS
+SELECT *
+FROM Actor
+UNION
+SELECT *
+FROM Director;
+
+DROP Table IF EXISTS WorksOn;
+CREATE TABLE WorksOn AS
+SELECT *
+FROM Starsin
+UNION
+SELECT *
+FROM Directs;
+
+-- A-Priori Step 1: Materialize the frequent 1-itemsets into a new table Baskets1
+CREATE OR REPLACE VIEW Co_Occurences1 AS (
+  SELECT w1.nid AS Movie1, w2.nid AS Movie2
+  FROM WorksOn w1, WorksOn w2
+  WHERE EXISTS (
+    SELECT u1.name AS Person1, u2.name AS Person2 
+	  FROM UnionActorDirector u1, UnionActorDirector u2
+	  WHERE u1.nid = w1.nid 
+	  	AND u2.nid = w2.nid
+	    AND w1.nid = w2.nid 
+	  GROUP BY u1.name, u2.name, w1.nid 
+	  HAVING COUNT(u1.name)>=4)
+  );
+
+
+
+
+
+-- Exercise 3 d) ----------------------------------------------------------------------
